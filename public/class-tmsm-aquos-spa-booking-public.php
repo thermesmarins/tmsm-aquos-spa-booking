a2d799eb9f72b717880ee1d04336d085221d9e82
Nico Mollet <nico.mollet@gmail.com>
2021-07-20 10:11:41 +0200

    Curl limit timetout to 10 seconds

--- public/class-tmsm-aquos-spa-booking-public.php
+++ public/class-tmsm-aquos-spa-booking-public.php
@@ -2170,6 +2170,7 @@ class Tmsm_Aquos_Spa_Booking_Public {
 			$ch = curl_init();
 			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, true );
 			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+			curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
 			curl_setopt( $ch, CURLOPT_URL, $settings_webserviceurl );
 			$result = curl_exec( $ch );
 			curl_close( $ch );
