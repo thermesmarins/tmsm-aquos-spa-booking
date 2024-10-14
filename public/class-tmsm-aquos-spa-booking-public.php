0b63873aedcef51a5d313668574177ac471def98
aflamentTM <aflament@thalasso-saintmalo.com>
2024-10-14 16:19:38 +0200

    fix code

--- public/class-tmsm-aquos-spa-booking-public.php
+++ public/class-tmsm-aquos-spa-booking-public.php
@@ -1717,14 +1717,12 @@ class Tmsm_Aquos_Spa_Booking_Public
 
 			if (!empty($message)) {
 				echo '<p>' . nl2br(esc_html($message)) . '</p>';
-				if ($in_date_range === true && $voucher === true) {
-					$price_change_information_notice = get_option('tmsm_aquos_spa_booking_messagestrong', false);
-					$price_change_information = get_option('tmsm_aquos_spa_booking_message', false);
-					echo '<p><strong>' . nl2br(esc_html($price_change_information_notice)) . ' ' .  nl2br(esc_html($price_change_information)) .'</strong></p>';
-					
-				}
 			}
-
+			if ($in_date_range === true && $voucher === true) {
+				$price_change_information_notice = get_option('tmsm_aquos_spa_booking_messagestrong', false);
+				$price_change_information = get_option('tmsm_aquos_spa_booking_message', false);
+				echo '<p><strong>' . nl2br(esc_html($price_change_information_notice)) . ' ' .  nl2br(esc_html($price_change_information)) . '</strong></p>';
+			}
 
 			if ($plain_text || !is_a($order, 'WC_Order')) {
 				return;
